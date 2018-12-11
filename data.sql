2909ee3dc4c6ae0b9be4dff81176e6a7096e3427
gouzouni <gouzouni@gmail.com>
2018-12-11 01:31:25 +0000

    Adding data.sql for pqsl console.

---
+++ data.sql
@@ -0,0 +1,38 @@
+create schema amazon;
+
+select nspname from pg_catalog.pg_namespace;
+
+CREATE TABLE amazon.amazonjobs (
+	job_id text NULL,
+	title text NULL,
+	department text NULL,
+	"location" text NULL,
+	to_date date NULL,
+	to_timestamp timestamp NULL,
+	url text NULL,
+	company_intro text NULL,
+	role_description text NULL,
+	listings text NULL
+);
+-- CREATE TABLE amazon.amazonjobs ( job_id text NULL, title text NULL, department text NULL, "location" text NULL, to_date date NULL, to_timestamp timestamp NULL, url text NULL, company_intro text NULL, role_description text NULL, listings text NULL ); 
+
+SELECT * FROM pg_catalog.pg_tables;
+
+SET CLIENT_ENCODING TO 'utf8';
+
+\COPY amazon.amazonjobs FROM 'C:/Users/gouzouni/PycharmProjects/amazonjobs/amazon.csv' WITH DELIMITER ‘,’ CSV HEADER;
+
+create table  amazon.amazonjobs_2
+as select 
+	am.job_id,
+	am.title,
+	am.department,
+	am."location",
+	am.to_date,
+	am.to_timestamp,
+	am.url,
+	am.company_intro,
+	am.role_description,
+	replace(am.listings, '\', ' back_splash ') listings
+from amazon.amazonjobs am;
+-- create table amazon.amazonjobs_2 as select am.job_id, am.title, am.department, am."location", am.to_date, am.to_timestamp, am.url, am.company_intro, am.role_description, replace(am.listings, '\', ' back_splash ') listings from amazon.amazonjobs am;
